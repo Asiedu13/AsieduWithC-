7b3038befe179f0381f8bfe98cb1c40b2a6fd38d
Prince Asiedu <princekofasiedu@gmail.com>
2022-10-19 16:18:39 +0000

    Learnt about the <iostream> library and its components

---
+++ inputAndOutput/inputoutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Enter two numbers" << std::endl;
+    int n1 = 0, n2 = 0;
+    std::cin >> n1 >> n2 ;
+
+    std::cout << "The sum of the two numbers is " << n1 + n2 << std::endl;
+    return 0;
+}
